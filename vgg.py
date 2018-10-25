c6c9f430fa3b7c69430a72bb7d54bc0d428e73e1
Vincent Valentine Jansen <vincentvalentine3636@yahoo.com>
2018-10-25 10:52:26 +0700

    first commit

--- vgg.py
+++ vgg.py
@@ -8,10 +8,10 @@ from keras.applications import imagenet_utils
 from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
 from keras.preprocessing.image import load_img, img_to_array, ImageDataGenerator
 from keras.layers import Dense, GlobalAveragePooling2D
-
+from keras.callbacks import TensorBoard
 from keras.models import Model, load_model
 from keras.optimizers import Adam
-
+from datetime import datetime
 
 #model = VGG16()
 
@@ -44,10 +44,11 @@ IMG_HEIGHT = 224
 
 NUM_EPOCHS = 1
 BATCH_SIZE = 1
-#BATCH_SIZE_VAL = 16
+BATCH_SIZE_VAL = 1
 FC_LAYER_SIZE = 1024
 
 MODEL_DIRECTORY = "VGG16-plant.h5"
+LOG_DIR = "logs"
 
 def get_nb_files(directory):
 	if not os.path.exists(directory):
@@ -67,12 +68,13 @@ def add_new_last_layer(model, num_classes):
   new_model = Model(input=model.input, output=predictions)
   return new_model
 
-def train(train_dir):
+def train(train_dir, val_dir):
 	
 	num_train_samples = get_nb_files(train_dir)
 	#print(num_train_samples)
 	num_classes = len(glob.glob(train_dir + "/*"))
 	#print(num_classes)
+	num_val_samples = get_nb_files(val_dir)
 
 	train_datagen =  ImageDataGenerator(
 		preprocessing_function=preprocess_input,
@@ -84,10 +86,25 @@ def train(train_dir):
 		horizontal_flip=True
 	)
 
+	test_datagen = ImageDataGenerator(
+		preprocessing_function=preprocess_input,
+		rotation_range=30,
+		width_shift_range=0.2,
+		height_shift_range=0.2,
+		shear_range=0.2,
+		zoom_range=0.2,
+		horizontal_flip=True)
+
 	train_generator = train_datagen.flow_from_directory(
 		train_dir,
 		target_size=(IMG_WIDTH, IMG_HEIGHT),
 		batch_size=BATCH_SIZE,
+	)
+
+	validation_generator = test_datagen.flow_from_directory(
+		val_dir,
+		target_size=(IMG_WIDTH, IMG_HEIGHT),
+		batch_size=BATCH_SIZE_VAL,
 	)
 
 	base_model = VGG16(weights='imagenet', include_top=False)
@@ -106,28 +123,28 @@ def train(train_dir):
 	# model.load_weights(MODEL_DIRECTORY, by_name=False)
 	# print(model)	
 
+	tensorboard = TensorBoard(log_dir="{}/{}".format(LOG_DIR, datetime.now().strftime('%Y%m%d-%H%M%S')))
+
 	history_transfer_learning = model.fit_generator(
 		train_generator,
 		nb_epoch=NUM_EPOCHS,
 		samples_per_epoch=num_train_samples,
-		#validation_data=validation_generator,
-		#nb_val_samples=num_val_samples,
+		callbacks=[tensorboard],
+		validation_data=validation_generator,
+		nb_val_samples=num_val_samples,
 		class_weight='auto')
 
 	model.save(MODEL_DIRECTORY)
 
-	image = load_img('Data/Ca (1).JPG', target_size=(224, 224))
-	image = img_to_array(image)
+	#image = load_img('Data/Ca (1).JPG', target_size=(224, 224))
+	#image = img_to_array(image)
 
-	x = np.expand_dims(image, axis=0)
-	x = preprocess_input(x)
-	features = model.predict(x)
-	print(features)
-	print(np.argmax(features))
+	#x = np.expand_dims(image, axis=0)
+	#x = preprocess_input(x)
+	#features = model.predict(x)
+	#print(features)
+	#print(np.argmax(features))
 	#label = imagenet_utils.decode_predictions(features)
-
-	#rint(label)
-
 
 	#image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 
@@ -143,9 +160,9 @@ def train(train_dir):
 	#print('%s (%.2f%%)' % (label[1], label[2]*100))
 
 def main():
-	train_dir = "Data/Tanaman - Copy/"
-	#val_dir = "Data/Tanaman/"
-	train(train_dir)
+	train_dir = "Data/Tanaman/"
+	val_dir = "Data/Tanaman_Validation/"
+	train(train_dir, val_dir)
 
 if __name__=="__main__":
 	main()
